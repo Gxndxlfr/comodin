c11daea8cde77909aa7483de3d8a513d8d79587c
Israel Guzman <israel.guzman@usach.cl>
2022-01-25 02:18:55 -0300

    [Israel Guzman] - relacion entre modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    public function role(){
+        return $this->hasMany(Role::class);
+    }
+
 
     /**
      * The attributes that are mass assignable.
